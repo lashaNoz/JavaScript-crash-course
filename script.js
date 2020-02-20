d10bf0504d3b250f5814029593986c133cc637fa
unknown <George>
2020-02-20 15:42:00 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// console.log("Hello world");
+
+// var person = "John";
+// console.log(person);
+
+// person = "Nick";
+// console.log(person);
+
+// var first-Name = "Bob";
+
+// var LastName = "Doe";
+
+// let age = 30;
+
+// console.log(age);
+
+// age = 35;
+
+// console.log(age);
+
+// const city = "London";
+
+// console.log(city);
+
+// const str = "John is a developer";
+// console.log(str);
+// console.log(typeof str);
+
+// const num = "27.23";
+// console.log(num);
+// console.log(typeof num);
+
+// const num1 = 10;
+// const num2 = 20;
+// const bool = true;
+// console.log(typeof bool);
+
+// let a;
+// console.log(a);
+// console.log(typeof a);
+
+// const b = null;
+// console.log(b);
